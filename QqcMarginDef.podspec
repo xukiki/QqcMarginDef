1274a7d62ea4c07f7ee011cc723ea273022fcc7e
qqc <qqc@admindeiMac.local>
2017-05-14 17:07:48 +0800

    完善

---
+++ QqcMarginDef.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+
+  s.license      = "MIT"
+  s.author       = { "qqc" => "20599378@qq.com" }
+  s.platform     = :ios, "8.0"
+  s.requires_arc  = true
+
+  s.name         = "QqcMarginDef"
+  s.version      = "1.0.0"
+  s.summary      = "QqcMarginDef"
+  s.homepage     = "https://github.com/xukiki/QqcMarginDef"
+  s.source       = { :git => "https://github.com/xukiki/QqcMarginDef.git", :tag => "#{s.version}" }
+  
+  s.source_files  = ["QqcMarginDef/*.{h,m}"]
+
+end
---
+++ QqcMarginDef/QqcMarginDef.h
@@ -0,0 +1,32 @@
+//
+//  QqcMarginDef.h
+//  QqcBaseFramework
+//
+//  Created by qiuqinchuan on 15/2/4.
+//  Copyright (c) 2015年 Qqc. All rights reserved.
+//
+
+#ifndef QqcBaseFramework_QqcMarginDef_h
+#define QqcBaseFramework_QqcMarginDef_h
+
+//间距定义
+//上下间距尺寸间隔
+#define margins_vert32_qqc 32
+#define margins_vert24_qqc 24
+#define margins_vert20_qqc 20
+#define margins_vert16_qqc 16
+#define margins_vert12_qqc 12
+#define margins_vert8_qqc  8
+#define margins_vert4_qqc  4
+
+//左右间距尺寸间隔
+#define margins_hori32_qqc 32
+#define margins_hori24_qqc 24
+#define margins_hori20_qqc 20
+#define margins_hori16_qqc 16
+#define margins_hori12_qqc 12
+#define margins_hori8_qqc  8
+#define margins_hori4_qqc  4
+//...间距定义 end...
+
+#endif
